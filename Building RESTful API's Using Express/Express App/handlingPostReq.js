423e34f40137c3b40db81537d476446a3250bc9e
jaykulkarni2620 <kulkarnijay76@gmail.com>
2023-12-09 10:03:58 +0530

    post req

--- Building RESTful API's Using Express/Express App/handlingPostReq.js
+++ Building RESTful API's Using Express/Express App/handlingPostReq.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.json());
+
 
 //as easy 
 // const port = 3000;
