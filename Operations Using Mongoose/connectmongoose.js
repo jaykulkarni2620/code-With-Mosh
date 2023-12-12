63f4a9cadcb9e87f5dcba390580e7f8120eb8d3d
jaykulkarni2620 <kulkarnijay76@gmail.com>
2023-12-12 18:02:46 +0530

    schemas

--- Operations Using Mongoose/connectmongoose.js
+++ Operations Using Mongoose/connectmongoose.js
@@ -3,3 +3,17 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/playground')
 .then(() => console.log("connected to mongoose"))
 .catch(error => console.log("Could not connect to MongoDB...", error));
+
+
+//schemas
+
+const courseSchemas = new mongoose.Schema({
+
+    name: String,
+    author: String,
+    tags: [String],
+    date : { type: Date, default: Date.now },
+    isPublished : Boolean
+
+
+});
