a605cfd63ffd0b32d53f67b830f4758582f0c102
jaykulkarni2620 <kulkarnijay76@gmail.com>
2023-12-20 12:37:01 +0530

    delete api with joi validation

--- TestMosh/index.js
+++ TestMosh/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -12,19 +13,35 @@ mongoose.connect('mongodb://localhost:27017/user', { useNewUrlParser: true, useU
     .catch(error => console.log("Could not connect to MongoDB...", error));
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    lname: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    lname: {
+        type: String,
+        required: true,
+    },
 });
 
 const User = mongoose.model('user', userSchema);
 
+// Joi validation schema
+const joiUserSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    lname: Joi.string().min(3).required(),
+});
+
 // Create a new user
 app.post('/users', async (req, res) => {
     try {
+         // Validate request data using Joi
+         const { error } = joiUserSchema.validate(req.body);
+         if (error) {
+             return res.status(400).json({ error: error.details[0].message });
+         }
+
         const { name, lname } = req.body;
         const newUser = new User({ name, lname });
-        console.log("name", name);
-        console.log("lname", lname);
         const result = await newUser.save();
         res.json(result);
     } catch (error) {
@@ -36,6 +53,7 @@ app.post('/users', async (req, res) => {
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find();
+        if (!users) return res.status(400).send("there is no data in database");
         res.json(users);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -46,6 +64,7 @@ app.get('/users', async (req, res) => {
 app.get('/users/:id', async (req,res) => {
     try{
         const user = await User.findById(req.params.id);
+        if (!user) return res.status(400).send("the given user id is not exist in databas.");
         res.json(user);
         console.log("reqParams",req.params.id);
     }catch (error) {
@@ -54,15 +73,35 @@ app.get('/users/:id', async (req,res) => {
 });
 
 // Update a user by ID
-app.put('/user/:id', async (req, res) => {
+app.put('/users/:id', async (req, res) => {
     try{
+        const { error } = joiUserSchema.validate(req.body);
+        if(error){
+            return res.status(400).json({ error: error.details[0].message });
+        };
         const { name, lname} = req.body;
         const updateUser = await User.findByIdAndUpdate(req.params.id,  { name, lname},  { new: true });
+        if (!updateUser) return res.status(400).send("the given user id is not exist in databas.");
         res.send(updateUser);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json( {error: error.message });
     }
 });
+
+//delete user by id 
+
+app.delete('/users/:id', async( req, res ) => {
+    try{
+        const delteUser = await User.findByIdAndDelete(req.params.id);
+        if (!delteUser) return res.status(400).send("the given user id is not exist in databas.");
+        res.send(delteUse);
+
+    } catch (error) {
+        res.status(500).json( {error: error.message});
+        console.log("ERROR:",error);
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
