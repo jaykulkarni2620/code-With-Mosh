e12c4badf66f7d2162c9b672aa6c2dc2747225a0
jaykulkarni2620 <kulkarnijay76@gmail.com>
2023-12-29 22:39:57 +0530

    adding user route in main index

--- Vidly/index.js
+++ Vidly/index.js
@@ -3,6 +3,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/users', users);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
