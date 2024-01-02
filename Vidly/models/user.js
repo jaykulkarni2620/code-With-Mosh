da8e49c7a6014938e9771b714d29071d98743e3b
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-03 00:58:19 +0530

    log in and getting data using get rout

--- Vidly/models/user.js
+++ Vidly/models/user.js
@@ -1,4 +1,6 @@
-const Joi = require('@hapi/joi');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -8,36 +10,38 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 50
   },
-
   email: {
     type: String,
     required: true,
-    unique: true,
     minlength: 5,
-    maxlength: 250,
+    maxlength: 255,
+    unique: true
   },
-
   password: {
     type: String,
     required: true,
-    unique: true,
     minlength: 5,
-    maxlength: 1025,
-  }
+    maxlength: 1024
+  },
+  isAdmin: Boolean
 });
+
+userSchema.methods.generateAuthToken = function() { 
+  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+  return token;
+}
 
 const User = mongoose.model('User', userSchema);
 
-function validateUser(User) {
-  const schema = Joi.object({
+function validateUser(user) {
+  const schema = {
     name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(3).max(250).email().required(),
+    email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required()
-  });
+  };
 
-  return Joi.validate(User, schema);
+  return Joi.validate(user, schema);
 }
 
-    
 exports.User = User; 
 exports.validate = validateUser;
--- Vidly/routes/users.js
+++ Vidly/routes/users.js
@@ -1,29 +1,32 @@
+const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
 router.post('/', async (req, res) => {
-    //checking that user passing correct data or not in ody
-    const { error } = validate(req.body);
-    if(error) return res.status(400).send(error.message);
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // the user already exist in dataase or notusing findoOne cz we cannot find y "id"
-    let user = await User.findOne({ email: req.body.email});
-    if(user) return res.status(400).send("user is already resgister");  
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered.');
 
-    // if user is not register
-    user = new User ({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    await user.save();
-    res.send({
-        name: User.name,
-        email: User.email
-    }
-   );
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router; 
