a341f05f2530a37ba03d0a5586732b4ed397a682
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-06 11:56:49 +0530

    Testing Number

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,3 +1,20 @@
-test('absolute', () => {
-    throw new Error('Something went wrong here');
+const lib = require('../lib')
+
+// test('absolute', () => {
+//     throw new Error('Something went wrong here');
+// });
+
+test('absolute - should return a positive number if input is positive', () => {
+   const result = lib.absolute(1);
+    expect(result).toBe(1);
 });
+
+test('absolute - should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+     expect(result).toBe(1);
+ });
+
+ test('absolute - should return 0 if input is 0', () => {
+    const result = lib.absolute(1);
+     expect(result).toBe(1);
+ });
