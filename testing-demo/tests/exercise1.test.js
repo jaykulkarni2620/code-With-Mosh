2da1ed321c4194acd54c1b075c819eb5f6fb149d
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-08 10:06:36 +0530

    FIzzBuzz Exercise

--- testing-demo/tests/exercise1.test.js
+++ testing-demo/tests/exercise1.test.js
@@ -8,21 +8,21 @@ describe('fizzBuzz', () => {
     
     it('Should return Fizzbuzz if input is divisible by 3 and 5', () => {
        const result =  lib.fizzBuzz(15);
-      except(result).toBe("FizzBuzz")
+       expect(result).toBe("FizzBuzz")
     })  
 
     it('Should return Fizz if input is divisible by 3', () => {
         const result =  lib.fizzBuzz(3);
-       except(result).toBe("Fizz")
+        expect(result).toBe("Fizz")
      })  
 
      it('Should return Fizz if input is divisible by 5', () => {
         const result =  lib.fizzBuzz(5);
-       except(result).toBe("Buzz")
+        expect(result).toBe("Buzz")
      })  
 
      it('Should return input if it is not divisible by 3 or 5', () => {
         const result =  lib.fizzBuzz(1);
-       except(result).toBe(1);
+        expect(result).toBe(1);
      })  
 })
