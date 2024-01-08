93d6714d5f3a5500174b3751c894b5eef337dfe0
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-08 10:24:39 +0530

    resolve this Deprecated: .handleExceptions() will be removed in winston@4. Use .exceptions.handle()

--- Vidly/startup/logging.js
+++ Vidly/startup/logging.js
@@ -3,15 +3,15 @@ const winston = require('winston');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.handleExceptions(
+  winston.exceptions.handle(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+  );
+    process.on('unhandledRejection', (ex) => {
+      winston.error('Unhandled Rejection:', ex.message, ex);
+    });
   
-  process.on('unhandledRejection', (ex) => {
-    throw ex;
-  });
-  
-  winston.add(winston.transports.File, { filename: 'logfile.log' });
+    winston.add(new winston.transports.File({ filename: 'logfile.log' }));
   // winston.add(winston.transports.MongoDB, { 
   //   db: 'mongodb://localhost/vidly',
   //   level: 'info'
