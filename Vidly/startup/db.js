7e1f69f8821c9bdae929dd5f0a2bfa626dcebf49
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-08 10:32:09 +0530

    facing the error

--- Vidly/startup/db.js
+++ Vidly/startup/db.js
@@ -1,9 +1,14 @@
-const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-module.exports = function() {
+module.exports = async function () {
   const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+
+  try {
+    await mongoose.connect(db);
+    console.log(`Connected to ${db}...`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+    throw error; // Rethrow the error to indicate that the connection failed
+  }
+};
