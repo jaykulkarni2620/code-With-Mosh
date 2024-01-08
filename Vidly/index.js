a3c648751d7fdf27530e8e5ea90b30149e15eefe
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-08 10:26:23 +0530

    Extracting index file

--- Vidly/index.js
+++ Vidly/index.js
@@ -1,71 +1,14 @@
-require('express-async-errors');
 const winston = require('winston');
-const config = require('config');
-const error = require('./middleware/error');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
-//for uncaughtException
-process.on('uncaughtException', (ex) => {
-  console.log('We got an unhandled rejection');
-  winston.error(ex.message, ex);
-  process.exit(1);
-})
-
-//for handlException
-winston.handleException(
-  new winston.transports.File ({ filename: 'uncaughtException.log'}),
-
-  process.on('uncaughtRejection', (ex) => {
-    throw ex;
-  })
-)
-
-
-//using winston 
-// using winston
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-// for mongodb
-// winston.add(winston.transports.MongoDB, ({
-//   db: 'mongodb://localhost/vidly',
-//   level:'info'
-// }));
-
-
-const p = Promise.reject(new Error('something failed miserably!'));
-p.then(() => console.log('Done'));
-
-
-console.log('Loaded configuration:', config.util.getConfigSources());
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-// Error Middleware
-app.use(error);
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+module.exports = server;
