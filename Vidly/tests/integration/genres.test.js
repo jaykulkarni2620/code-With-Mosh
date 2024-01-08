5a33f65c19a5b5f651f1c242eb419be32c70e148
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-08 23:17:36 +0530

    Populating theTest DB

--- Vidly/tests/integration/genres.test.js
+++ Vidly/tests/integration/genres.test.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
+const { Genre } = require('../../models/genre');
+
 let server;
 
 describe('/api/genres', () => {
+
+
     // pratyek endpoint chya adhi server start honar
     beforeEach(async () => {
         server = await require('../../index');
@@ -15,8 +19,15 @@ describe('/api/genres', () => {
     
     describe('GET/', () => {
         it('should return all genres', async ()=> {
+           await Genre.collection.insertMany([
+                { name: 'Mohan chavan' },
+                { name: 'Genre2' },
+            ])
             const res = await request(server).get('/api/genres');
-            expect(res.status).toBe(200);
+            // expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'Mohan chavan')).toBeTruthy();  
         })
     })
-})
+ })
+
