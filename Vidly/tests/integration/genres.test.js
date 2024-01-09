71569a913051e003bd0f344da951418a453fddff
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-09 10:34:09 +0530

    Testing Invalid Inputs

--- Vidly/tests/integration/genres.test.js
+++ Vidly/tests/integration/genres.test.js
@@ -6,8 +6,6 @@ const { describe } = require('joi/lib/types/lazy');
 let server;
 
 describe('/api/genres', () => {
-
-
     // pratyek endpoint chya adhi server start honar
     beforeEach(async () => {
         server = await require('../../index');
@@ -33,34 +31,61 @@ describe('/api/genres', () => {
 //     })
 //  })
 
-       describe('GET/:id', () => { 
-    console.log('Server console', server);
-    it('should return a genre if valid id is passed', async () => {
-        const genre = new Genre({ name: 'genre1'});
-        await genre.save();
+    //    describe('GET/:id', () => { 
+    // console.log('Server console', server);
+    // it('should return a genre if valid id is passed', async () => {
+    //     const genre = new Genre({ name: 'genre1'});
+    //     await genre.save();
 
-        const res = await request(server).get('http://127.0.0.1:3000/api/genres/' + genre._id);
+    //     const res = await request(server).get('http://127.0.0.1:3000/api/genres/' + genre._id);
 
-        expect(res.status).toBe(200);
-        // expect(res.body).toMatchObject(genre);
+    //     expect(res.status).toBe(200);
+    //     // expect(res.body).toMatchObject(genre);
        
-        // return to buffer so using this toHaveproperty()
-        except(res.body).toHaveProperty('name', genre.name);
-     })
+    //     // return to buffer so using this toHaveproperty()
+    //     except(res.body).toHaveProperty('name', genre.name);
+    //  })
 
-     it('should return a 404 if invalid id', async () => {
+    //  it('should return a 404 if invalid id', async () => {
 
-        const res = await request(server).get('api/genres/1');
+    //     const res = await request(server).get('api/genres/1');
 
-        expect(res.status).toBe(404);
-     })
-    })
+    //     expect(res.status).toBe(404);
+    //  })
+    // })
 
     //POST API
         describe('POST/', () => {
-            it('should return 401 if client is not looged in', async() => {
-               const res = await request(sever).post('/api/genres').send({ name: 'genre1'});
-                except(res.status).toBe(401);
+            // it('should return 401 if client is not looged in', async() => {
+
+            //     const token = new User().generateAtuhToken();
+
+            //    const res = await (await request(sever)
+            //    .post('/api/genres'))
+            //    .send({ name: 'genre1'});
+            //     except(res.status).toBe(401);
+            // })
+
+            it('should return 400 if genre is less than 5 characters', async() => {
+               const res =  await request(server)
+               .post('/api/genres')
+               .set('X-auth-token', token)
+               .send({ name: '1234'});
+
+
+                except(res.status).toBe(400);
             })
+
+
+            const name = new Array(52).join('a');
+            it('should return 400 if genre is more than 50 characters', async() => {
+                const res =  await request(server)
+                .post('/api/genres')
+                .set('X-auth-token', token)
+                .send({ name: name});
+ 
+ 
+                 except(res.status).toBe(400);
+             })
         })
 })
