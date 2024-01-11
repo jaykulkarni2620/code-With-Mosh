9711596d7e268e2d72aff2ac9fa78924cce61003
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-11 23:38:32 +0530

    Looking up an Object

--- Vidly/routes/returns.js
+++ Vidly/routes/returns.js
@@ -4,10 +4,15 @@ const router = express.Router();
 
 
 router.post('/', async (req, res) => {
-    if(req.body.customerId) return res.status(400).send('Customer id not exist');
-    if(req.body.movieId) return res.status(400).send('movie id not exist');
+    if(!req.body.customerId) return res.status(400).send('Customer id not exist');
+    if(!req.body.movieId) return res.status(400).send('movie id not exist');
 
-    res.status(401).send('Unauthorised');
+   const rental = await Rental.findOne({
+      'customer._id': req.body.customerId,
+      'movie._id': req.body.customerId,
+    })
+
+    if(!rental) return res.status(404).send('Rental not Found');
   });
 
   module.exports = router
