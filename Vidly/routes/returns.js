0d997816366193cbd5f6fd2f03919c2b1ec889f2
jaykulkarni2620 <kulkarnijay76@gmail.com>
2024-01-15 22:41:01 +0530

    Refactoring the validation create a middlwear

--- Vidly/routes/returns.js
+++ Vidly/routes/returns.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const { Rental } = require('../models/rental');
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
 const router = express.Router();
 const moment = require('moment');
 const { Movie } = require('../models/movie');
+const Joi = require('joi');
 
 
 
-router.post('/', auth, async (req, res) => {
-    if(!req.body.customerId) return res.status(400).send('Customer id not exist');
-    if(!req.body.movieId) return res.status(400).send('movie id not exist');
+
+router.post('/', [ auth, validate ], async (req, res) => {
+    // if(!req.body.customerId) return res.status(400).send('Customer id not exist');
+    // if(!req.body.movieId) return res.status(400).send('movie id not exist');
+
+   // Refactoring 
+    // const { error } = validateReturn(req.body); 
+    // if (error) return res.status(400).send(error.details[0].message);
+  
 
    const rental = await Rental.findOne({
       'customer._id': req.body.customerId,
@@ -34,5 +42,14 @@ router.post('/', auth, async (req, res) => {
     return res.status(200).send(rental);
 
   });
+
+  function validateReturn(genre) {
+    const schema = {
+      customerId: Joi.ObjectId().required(),
+      movieId: Joi.ObjectId().required()
+    };
+  
+    return Joi.validate(genre, schema);
+  }
 
   module.exports = router
